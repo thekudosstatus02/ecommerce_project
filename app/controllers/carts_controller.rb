c99e85f006134f3a3dafe65bdc6a4b566d5d237f
Kajal <65011099+kajal-610@users.noreply.github.com>
2023-02-27 11:03:47 +0530

    Update carts_controller.rb

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,9 +3,7 @@ class CartsController < ApplicationController
   def index
     @carts = Cart.all
   end
-
-  # GET /carts/1
-  # GET /carts/1.json
+	
   def show
 	begin
 	@cart = Cart.find(params[:id])
@@ -21,17 +19,13 @@ class CartsController < ApplicationController
 
   end
 
-  # GET /carts/new
   def new
     @cart = Cart.new
   end
 
-  # GET /carts/1/edit
   def edit
   end
 
-  # POST /carts
-  # POST /carts.json
   def create
     @cart = Cart.new(cart_params)
 
@@ -46,8 +40,6 @@ class CartsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /carts/1
-  # PATCH/PUT /carts/1.json
   def update
     respond_to do |format|
       if @cart.update(cart_params)
@@ -60,8 +52,6 @@ class CartsController < ApplicationController
     end
   end
 
-  # DELETE /carts/1
-  # DELETE /carts/1.json
   def destroy
     @cart.destroy
     respond_to do |format|
@@ -71,12 +61,10 @@ class CartsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_cart
       @cart = Cart.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params[:cart]
     end
